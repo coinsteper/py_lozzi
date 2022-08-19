7b6e41c459fabc7f9f2402a2fb1f03a6335c021f
coinsteper <109713244+coinsteper@users.noreply.github.com>
2022-08-19 18:10:10 +0900

    대대적 수정

--- auto_lozzi.py
+++ auto_lozzi.py
@@ -7,34 +7,36 @@ import time
 import random
 import os
 
-img_box = r'C:\python\image_processing\auto_lozzi\box.png'
-img_more_box_g = r'C:\python\image_processing\auto_lozzi\more_box_g.png'
-img_more_box = r'C:\python\image_processing\auto_lozzi\more_box.png'
-img_gold_box = r'C:\python\image_processing\auto_lozzi\goldbox.png'
-img_ok = r'C:\python\image_processing\auto_lozzi\ok.png'
-img_x = r'C:\python\image_processing\auto_lozzi\x.png'
-img_x2 = r'C:\python\image_processing\auto_lozzi\x2.png'
-img_x3 = r'C:\python\image_processing\auto_lozzi\x3.png'
-img_x4 = r'C:\python\image_processing\auto_lozzi\x4.png'
-img_x5 = r'C:\python\image_processing\auto_lozzi\x5.png'
-img_x6 = r'C:\python\image_processing\auto_lozzi\x6.png'
-img_x7 = r'C:\python\image_processing\auto_lozzi\x7.png'
-img_x8 = r'C:\python\image_processing\auto_lozzi\x8.png'
-img_x9 = r'C:\python\image_processing\auto_lozzi\x9.png'
-img_skip = r'C:\python\image_processing\auto_lozzi\skip.png'
-img_skip1 = r'C:\python\image_processing\auto_lozzi\skip1.png'
-img_close = r'C:\python\image_processing\auto_lozzi\close.png'
+img_box = r'C:\python\image_processing\auto_lozzi\img\box.png'
+img_more_box_g = r'C:\python\image_processing\auto_lozzi\img\more_box_g.png'
+img_more_box = r'C:\python\image_processing\auto_lozzi\img\more_box.png'
+img_gold_box = r'C:\python\image_processing\auto_lozzi\img\goldbox.png'
+img_ok = r'C:\python\image_processing\auto_lozzi\img\ok.png'
 
-img_list = [img_box, img_more_box_g, img_more_box, img_gold_box, img_ok, img_x, img_x2, img_x3, img_x4, img_x5, img_x6, img_x7, img_x8,img_x9, img_skip, img_skip1
-]
+#img_x = r'C:\python\image_processing\auto_lozzi\img\x.png'
+#img_x2 = r'C:\python\image_processing\auto_lozzi\img\x2.png'
+#img_x3 = r'C:\python\image_processing\auto_lozzi\img\x3.png'
+#img_x4 = r'C:\python\image_processing\auto_lozzi\img\x4.png'
+#img_x5 = r'C:\python\image_processing\auto_lozzi\img\x5.png'
+#img_x6 = r'C:\python\image_processing\auto_lozzi\img\x6.png'
+#img_x7 = r'C:\python\image_processing\auto_lozzi\img\x7.png'
+#img_x8 = r'C:\python\image_processing\auto_lozzi\img\x8.png'
+#img_x9 = r'C:\python\image_processing\auto_lozzi\img\x9.png'
 
-img_control = r'C:\python\image_processing\auto_lozzi\control.png'
-img_switch = r'C:\python\image_processing\auto_lozzi\switch.png'
+img_path_x_list = r'C:\python\image_processing\auto_lozzi\img\x'
+img_skip = r'C:\python\image_processing\auto_lozzi\img\skip.png'
+img_skip1 = r'C:\python\image_processing\auto_lozzi\img\skip1.png'
+img_close = r'C:\python\image_processing\auto_lozzi\img\close.png'
+
+img_x_list = os.listdir(img_path_x_list)
+
+img_control = r'C:\python\image_processing\auto_lozzi\img\control.png'
+img_switch = r'C:\python\image_processing\auto_lozzi\img\switch.png'
 #ImageGrab.grab(bbox=None, include_layered_windows=True)
 
 tvwindow = pg.getWindowsWithTitle('Xiaomi_Mi A1_unknown - TeamViewer')
 left_g = tvwindow[0].left + 500
-top_g = tvwindow[0].top + 200
+top_g = tvwindow[0].top + 150
 width_g = tvwindow[0].width - 400
 height_g = tvwindow[0].height - 100
 start_time = time.time()
@@ -60,10 +62,20 @@ def find_and_click(img, pos=0):
     if find_img != None:
         time.sleep(0.01)
 
+        # 더 받기 랜덤한 좌표 클릭해서 봇체크 우회
         if img == img == img_more_box or img == img_more_box_g:
             find_img = (find_img.left + random.randrange(-5, 5), find_img.top + random.randrange(-5, 5), find_img.width,
                         find_img.height)
             time.sleep(0.01)
+        
+        #x 잘못 클릭해서 링크로 빠지는거 방지
+        if '_x_' in img:
+            
+            while True:
+                time.sleep(1)
+                find_img = pg.locateOnScreen(img, confidence=confi, region=(left_, top_, width_, height_))
+                if find_img != None:
+                    break
 
         pg.click(find_img, clicks=1, duration=0.1)
 
@@ -71,7 +83,7 @@ def find_and_click(img, pos=0):
             while True:
                 find_img = pg.locateOnScreen(img, confidence=confi, region=(left_, top_, width_, height_))
                 if find_img != None:
-                    pg.click(find_img, clicks=2, duration=0.1)
+                    pg.click(find_img, clicks=1, duration=0.1)
                 else:
                     break
 
@@ -87,17 +99,25 @@ def make_screenshot():
     path = os.getcwd()
     filename = path + '\\screenshot\\' + timestr + '.png'
     print(filename)
-    pg.screenshot(filename, region=(left_g, top_g, width_g-200, height_g-200))
+    pg.screenshot(filename, region=(left_g, top_g, width_g-350, height_g-110))
 
 
 def make_switch():
+    start_time = time.time()
+
     while True:
+        if time.time() - start_time >= 20:
+            break
+
         time.sleep(0.2)
 
         if find_and_click(img_control):
             time.sleep(0.5)
 
             while True:
+                if time.time() - start_time >= 20:
+                    break
+
                 time.sleep(0.2)
                 if find_and_click(img_switch):
                     start_time = time.time()
@@ -115,6 +135,7 @@ if __name__ =='__main__':
                 time.sleep(1)
                 make_screenshot()            
             find_and_click(img_ok)
+            '''
             find_and_click(img_x)
             find_and_click(img_x2)
             find_and_click(img_x3)
@@ -124,6 +145,13 @@ if __name__ =='__main__':
             find_and_click(img_x7)
             find_and_click(img_x8)
             find_and_click(img_x9)
+            find_and_click(img_x11)
+            '''
+
+            # x는 동적으로 추가되더라도 잘 실행 되기 위해
+            for i in img_x_list:
+                find_and_click(img_path_x_list + '\\' + i, 1)
+
             find_and_click(img_skip, 1)
             find_and_click(img_skip1, 1)            
             find_and_click(img_close, 3)
@@ -132,10 +160,10 @@ if __name__ =='__main__':
             for i in img_list:
                 find_and_click(i)
             '''
-            '''
-            if time.time() - start_time >= 30:
+            
+            if time.time() - start_time >= 40:
                 make_switch()
-            '''
+            
             # 여기서 20초 넘으면 설정 클릭하고 홈 눌러서 강제로 더받기 박스 띄우기
 
     except KeyboardInterrupt:
